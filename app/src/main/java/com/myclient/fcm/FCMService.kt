a2cbf0f3668976b7e84a0b4cfb147ae10844f650
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-25 18:22:24 -0300

    Config channel added

--- app/src/main/java/com/myclient/fcm/FCMService.kt
+++ app/src/main/java/com/myclient/fcm/FCMService.kt
@@ -1,10 +1,22 @@
 package com.myclient.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import com.myclient.Constants
+import com.myclient.R
+import com.myclient.product.MainActivity
 
 class FCMService : FirebaseMessagingService() {
     override fun onNewToken(newToken: String) {
@@ -25,17 +37,19 @@ class FCMService : FirebaseMessagingService() {
         Log.i("new token", newToken)
     }
 
-//    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-//        super.onMessageReceived(remoteMessage)
-//
+    //recibir notificacion en primer plano
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
 //        if (remoteMessage.data.isNotEmpty()){
 //            sendNotificationByData(remoteMessage.data)
 //        }
-//
-//        remoteMessage.notification?.let {
+
+        //verificar que la notificacion no sea nula
+        remoteMessage.notification?.let {
 //            val imgUrl = it.imageUrl//"https://facturacionweb.site/blog/wp-content/uploads/2020/08/firebase_android.png"
 //            if (imgUrl == null){
-//                sendNotification(it)
+                sendNotification(it)
 //            } else {
 //                Glide.with(applicationContext)
 //                    .asBitmap()
@@ -51,28 +65,34 @@ class FCMService : FirebaseMessagingService() {
 //                        override fun onLoadCleared(placeholder: Drawable?) {}
 //                    })
 //            }
-//        }
-//    }
+        }
+    }
 
-//    private fun sendNotification(notification: RemoteMessage.Notification, bitmap: Bitmap? = null){
-//        val intent = Intent(this, OrderActivity::class.java)
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
-//            PendingIntent.FLAG_ONE_SHOT)
-//
-//        val channelId = getString(R.string.notification_channel_id_default)
-//        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-//        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-//            .setSmallIcon(R.drawable.ic_stat_name)
-//            .setContentTitle(notification.title)
-//            .setContentText(notification.body)
-//            .setAutoCancel(true)
-//            .setSound(defaultSoundUri)
-//            .setColor(ContextCompat.getColor(this, R.color.yellow_a400))
-//            .setContentIntent(pendingIntent)
+    //procesar el mensaje recibido
+    private fun sendNotification(notification: RemoteMessage.Notification
+    //                             , bitmap: Bitmap? = null
+    ){
+        //construir una notifcacion con kotlin
+        val intent = Intent(this, MainActivity::class.java)
+        //val intent = Intent(this, OrderActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)  //recomendacion de firebase
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+            //PendingIntent.FLAG_ONE_SHOT)
+            PendingIntent.FLAG_MUTABLE)
+
+        val channelId = getString(R.string.notification_channel_id_default)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setAutoCancel(true)  //eliminar la notificacion luego de darle click
+            .setSound(defaultSoundUri)
+//            .setColor(ContextCompat.getColor(this, R.color.amber_500_dark))
+            .setContentIntent(pendingIntent)
 //            .setStyle(NotificationCompat.BigTextStyle()
 //                .bigText(notification.body))
-//
+
 //        bitmap?.let {
 //            notificationBuilder
 //                .setLargeIcon(bitmap)
@@ -80,18 +100,19 @@ class FCMService : FirebaseMessagingService() {
 //                    .bigPicture(bitmap)
 //                    .bigLargeIcon(null))
 //        }
-//
-//        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-//            val channel = NotificationChannel(channelId,
-//                getString(R.string.notification_channel_name_default),
-//                NotificationManager.IMPORTANCE_DEFAULT)
-//            notificationManager.createNotificationChannel(channel)
-//        }
-//
-//        notificationManager.notify(0, notificationBuilder.build())
-//    }
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        //canal para notificacion en primer plano
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(channelId,
+                getString(R.string.notification_channel_name_default),
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
 
 //    private fun sendNotificationByData(data: Map<String, String>){
 //        val intent = Intent(this, OrderActivity::class.java)
