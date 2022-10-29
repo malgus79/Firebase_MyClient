24e3e6f7e0480bcb9257b4f75db648b0c3b70114
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-29 11:36:51 -0300

    Activicy and fragment settings added

---
+++ app/src/main/java/com/myclient/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.myclient.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myclient.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
---
+++ app/src/main/java/com/myclient/settings/SettingsFragment.kt
@@ -0,0 +1,37 @@
+package com.myclient.settings
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+import com.myclient.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        //config del switch
+        val switchPreferenceCompat = findPreference<SwitchPreferenceCompat>(getString(R.string.pref_offers_key))
+        switchPreferenceCompat?.setOnPreferenceChangeListener { preference, newValue ->
+            //verificar que sea de tipo boolean
+            (newValue as? Boolean)?.let { isChecked ->
+                val topic = getString(R.string.settings_topic_offers)
+                if (isChecked){
+                    Firebase.messaging.subscribeToTopic(topic)
+                        .addOnSuccessListener {
+                            Toast.makeText(context, "Notificaciones activadas", Toast.LENGTH_SHORT).show()
+                        }
+                } else {
+                    Firebase.messaging.unsubscribeFromTopic(topic)
+                        .addOnSuccessListener {
+                            Toast.makeText(context, "Notificaciones desactivadas", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+            true
+        }
+    }
+}
